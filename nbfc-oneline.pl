9fb52acdbd934c0d81ea505304783971effdc7ee
louisophie <louisophie@gmail.com>
2025-01-15 12:57:56 +0800

    20250115

--- nbfc-oneline.pl
+++ nbfc-oneline.pl
@@ -3,7 +3,7 @@ use strict;
 use utf8;
 use open ':std', ':encoding(UTF-8)';
 
-#`echo <password> | sudo -S nbfc start`
+#`echo <password> | sudo -S nbfc start`;
 `sudo nbfc start`;
 while(1){
 $_=` nbfc status -a` ;
